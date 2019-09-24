69e025a05d7b6c3a0dc7f19fb113e447cba84625
shuting yang <sophia12332111@gmail.com>
2019-09-23 22:25:11 -0400

    lab2 complete

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -9,6 +9,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main_linear);
+        //setContentView(R.layout.activity_main_relative);
+        setContentView(R.layout.activity_main_grid);
+//        addListenerOnButton();
     }
+
+//    public void addListenerOnButton() {
+//
+//        imageButton = (ImageButton) findViewById(R.id.imageButton1);
+//
+//        imageButton.setOnClickListener(new OnClickListener() {
+//
+//            @Override
+//            public void onClick(View arg0) {
+//
+//                Toast.makeText(MyAndroidAppActivity.this,
+//                        "ImageButton is clicked!", Toast.LENGTH_SHORT).show();
+//
+//            }
+//
+//        });
+//
+//    }
+
+
 }
